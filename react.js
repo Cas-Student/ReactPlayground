267037138d800e93e0cd1a129dbcfebf35b02df9
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-16 19:12:41 -0400

    Update react.js

--- react.js
+++ react.js
@@ -2,6 +2,6 @@ function LoadTitle() {
   return "My Cool Website";
 }
 
-const container = document.getElementById("main");
+const container = document.getElementById("title");
 const root = ReactDOM.createRoot(container);
 root.render("<LoadTitle />");
