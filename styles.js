937a6d83a61b9ef20cfb81500834ba4fc79ac896
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-05-16 19:45:00 -0400

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,1 +1,3 @@
-document.getElementsByClassName("title").innerHTML = "Javascript Website";
+//Title styles
+document.getElementsByClassName("title").style.textAlign = "center";
+document.getElementsByClassName("title").style.fontSize = "31.25px";
